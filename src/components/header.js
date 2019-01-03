5cf26d33d90e9bb103943dad682632567cc063f9
aubriehull <aubrieh97@gmail.com>
2019-01-02 17:32:00 -0700

    added header h1 and p styles

--- src/components/header.js
+++ src/components/header.js
@@ -6,9 +6,10 @@ const Header = () => {
         <div className="header__skew"></div>
         <div className="header__border"></div>
             <h1>{'Bottega Madlibs'}</h1>
-            <p>{'Fill out the fields below to generate the Mad Libs Story'}</p>
+            <p>{'Fill out the fields below and click the button to generate the Mad Libs story.'}</p>
         </div>
     )
+
 }
 
 export default Header;
